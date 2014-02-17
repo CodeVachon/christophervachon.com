451f68c91689b8f35964bb0167fdfa7ebc1732c8
Christopher Vachon <code@christophervachon.com>
2014-02-17 16:13:56 -0500

    Update Grunt for Less, JSHint, and CSSLint

--- gruntfile.js
+++ gruntfile.js
@@ -1,3 +1,55 @@
 module.exports = function(grunt) {
-	
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		jshint: {
+			options: {
+				globals: {
+					jQuery: true,
+					console: true,
+					module: true
+				} // close .globals
+			}, // close .options
+			file: {
+				src: ['gruntfile.js'],
+			}
+		}, 
+		less: {
+			build: {
+				options: {
+				},
+				files: {
+					"src/css/christophervachon.css": "src/less/christophervachon.less"
+				}
+			}
+		},
+		csslint: {
+			strict: {
+				options: {
+					import: 2
+				},
+				src: ['src/css/*.css']
+			}
+		},
+		watch: {
+			scripts: {
+				files: ['lib/js/*.js','gruntfile.js'],
+				tasks: ['jshint'],
+				options: {
+					spawn: false,
+				}
+			},
+			less: {
+				files: ['src/less/*.less'],
+				tasks: ['less:build','csslint:strict'],
+				options: {
+					spawn: false,
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-csslint');
 }; // close module.exports
