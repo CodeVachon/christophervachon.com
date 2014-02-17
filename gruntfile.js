c8c855d4522795311f35afb6705e5907a3b782fd
Christopher Vachon <code@christophervachon.com>
2014-02-17 16:51:30 -0500

    Added Uglify and JS Handling

--- gruntfile.js
+++ gruntfile.js
@@ -9,10 +9,13 @@ module.exports = function(grunt) {
 					module: true
 				} // close .globals
 			}, // close .options
-			file: {
+			gruntfile: {
 				src: ['gruntfile.js'],
+			},
+			sourceFiles: {
+				src: ['src/js/*.js'],
 			}
-		}, 
+		}, // close jshint
 		less: {
 			build: {
 				options: {
@@ -21,7 +24,7 @@ module.exports = function(grunt) {
 					"src/css/christophervachon.css": "src/less/christophervachon.less"
 				}
 			}
-		},
+		}, // close less
 		csslint: {
 			strict: {
 				options: {
@@ -29,30 +32,47 @@ module.exports = function(grunt) {
 				},
 				src: ['src/css/*.css']
 			}
-		},
+		}, // close csslint
 		cssmin: {
 			minify: {
 				files: {
 					'includes/css/christophervachon.min.css':['src/css/*.css','!src/css/*.min.css']
 				}
 			}
-		},
+		}, // close cssmin
+		uglify: {
+			sourceFiles: {
+				files: [{
+					expand: true,
+					cwd: 'src/js/',
+					src: '**/*.js',
+					dest: 'includes/js'
+				}]
+			}
+		}, // close uglify
 		watch: {
-			scripts: {
-				files: ['lib/js/*.js','gruntfile.js'],
-				tasks: ['jshint'],
+			gruntfile: {
+				files: ['gruntfile.js'],
+				tasks: ['jshint:gruntfile'],
 				options: {
-					spawn: false,
+					spawn: false
 				}
-			},
+			}, // close gruntfile
 			less: {
 				files: ['src/less/*.less'],
 				tasks: ['less:build','csslint:strict','cssmin:minify'],
 				options: {
-					spawn: false,
+					spawn: false
 				}
-			}
-		}
+			}, // close less
+			jsfiles: {
+				files: ['src/js/*.js'],
+				tasks: ['jshint:sourceFiles','uglify:sourceFiles'],
+				options: {
+					spawn: false
+				}
+			} // close jsfiles
+		} // close watch
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-watch');
@@ -60,4 +80,5 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-csslint');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 }; // close module.exports
