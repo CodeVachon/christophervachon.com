0b9337c32b25d7ca4df703ec463fd3d8b4a0024a
Christopher Vachon <code@christophervachon.com>
2014-07-27 11:58:55 -0400

    Added Search Back

--- src/js/cmvVirt.js
+++ src/js/cmvVirt.js
@@ -7,6 +7,18 @@ $(document).ready(function() {
 
 	$('.nav-socialmedia').on('click',".search",function(e) {
 		e.preventDefault();
-		alert("Search!");
+		$(this).toggleClass('active');
+		if ($(this).hasClass('active')) {
+			console.log("add form");
+			var _form = $('<form>').prop({"action":"/blog/search","method":"get"}).addClass("inline-search")
+				.append($('<input>').prop({"type":"text","name":"search_for","placeholder":"Search For..."}))
+				.append($('<button>').prop({"type":"submit"}).text("Search"));
+			_form.hide();
+			$(this).closest("li").append(_form);
+			_form.slideDown("fast");
+		} else {
+			console.log("remove form");
+			$(this).closest("li").find('form').slideUp("fast", function() { $(this).remove(); });
+		}
 	});
 });
